42d4e9c6e531f1cd13b98cdba0350606510ed7ef
Cleissom <cleissomfernandes@hotmail.com>
2019-04-04 16:51:29 -0300

    Update

--- 344 - Banker's Algorithm/Python/344_Bankers_Algorithm.py
+++ 344 - Banker's Algorithm/Python/344_Bankers_Algorithm.py
@@ -3,7 +3,14 @@ import re
 filename = '../input.txt'
 
 with open(filename) as file:
-    line = file.read()
+    lines = file.readlines()
 
-array = re.findall(r'[0-9] [0-9] [0-9]', line)
-print(array)
+intarray = [list(line.strip('[] \n\r').split()) for line in lines]
+
+size = len(intarray[1])
+middle = int(size/2)
+
+availible = int("".join(intarray[0]))
+
+processes = [(i, int("".join(intarray[i][0:middle])), int("".join(intarray[i][middle:]))) for i in range(1, len(intarray))]
+print(processes)
